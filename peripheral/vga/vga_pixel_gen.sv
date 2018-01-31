087b3904db544870afd4e3f5f39a9f42937b1afd
Mooneer Salem <mooneer.salem@tribusmedia.com>
2018-01-30 23:42:21 -0800

    Suppress VGA rendering of rows 25-29.
    Eventually we'll vertically center the display to compensate.

--- peripheral/vga/vga_pixel_gen.sv
+++ peripheral/vga/vga_pixel_gen.sv
@@ -21,14 +21,16 @@ output reg vga_g;
 output reg vga_b;
 
 wire pixel_en;
+wire ram_addr_past_80x25;
 
 assign ram_addr = data_reset ? 12'd0 : (pixel_col / 12'd8) + ((pixel_row / 12'd16) * 12'd80);
+assign ram_addr_past_80x25 = ram_addr > (80 * 25);
 
 always @(posedge pixel_clk)
 begin
-	vga_r <= data_reset ? 1'b0 : (!pixel_en ? ram_contents[14] : ram_contents[10]);
-	vga_g <= data_reset ? 1'b0 : (!pixel_en ? ram_contents[13] : ram_contents[9]);
-	vga_b <= data_reset ? 1'b0 : (!pixel_en ? ram_contents[12] : ram_contents[8]);
+	vga_r <= data_reset || ram_addr_past_80x25 ? 1'b0 : (!pixel_en ? ram_contents[14] : ram_contents[10]);
+	vga_g <= data_reset || ram_addr_past_80x25 ? 1'b0 : (!pixel_en ? ram_contents[13] : ram_contents[9]);
+	vga_b <= data_reset || ram_addr_past_80x25 ? 1'b0 : (!pixel_en ? ram_contents[12] : ram_contents[8]);
 end
 
 pc_vga_8x16 char_rom(data_reset ? 0 : pixel_clk, pixel_col[2:0], pixel_row[3:0], ram_contents[7:0], pixel_en);
