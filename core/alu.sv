7b207b504ce59573044f4755231051ad3b5d612d
Mooneer Salem <mooneer@gmail.com>
2018-02-08 22:49:30 -0800

    Added left shift to ALU.

--- core/alu.sv
+++ core/alu.sv
@@ -7,16 +7,24 @@ module alu(
 
 input operand1[15:0];
 input operand2[15:0];
-input operation[1:0];
+input operation[2:0];
 output result[15:0];
 
 always @(operand1 or operand2 or operation)
 begin
 	case (operation)
-		2'b00:	result <= operand1 + operand2;
-		2'b01:	result <= operand1 & operand2;
-		2'b10:	result <= operand1 | operand2;
-		2'b11:	result <= ~operand1;
+		3'b000:	
+			result <= operand1 << operand2;
+		3'b100:	
+			result <= operand1 + operand2;
+		3'b101:	
+			result <= operand1 & operand2;
+		3'b110:	
+			result <= operand1 | operand2;
+		3'b111:	
+			result <= ~operand1;
+		default:
+			result <= 0;
 	endcase
 end
 
