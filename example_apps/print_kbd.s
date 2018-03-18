b7756472a6a9d4e73c198fca2b20c3e4c80ab629
Mooneer Salem <mooneer@gmail.com>
2018-03-18 11:56:37 -0700

    Fixed syntax errors in keyboard test app and recompiled existing apps with new assembler.

--- example_apps/print_kbd.s
+++ example_apps/print_kbd.s
@@ -1,36 +1,34 @@
-main:
-	ld r1, r0, 0 ; Keyboard scan code register
-	ld r2, r0, 1 ; Video memory offset
-	ld r3, r1, 0 ; Load scan code into register
-	shr r4, r3, 4 ; Most significant nibble
-	and r4, r4, 0xF
-	sub r5, r4, 9
-	brg hex_out1
-	ld r5, r0, 2 ; ASCII 0
+.raw 0xC000				# Keyboard scan code register
+.raw 0xF82F				# Video memory offset
+.raw 0x0700				# BG/FG color
+
+start:
+	ld r1, r0, 0
+	ld r2, r0, 1
+	ld r3, r1, 0
+	shr r4, r3, 4
+	and r4, r4, 15
+	add r5, r4, -9
+	bgt hex_out1
+	ld r5, r0, 2 		# ASCII 0
 	br out_1
 hex_out1:
-	ld r5, r0, 3 ; ASCII A
+	ld r5, r0, 3		# ASCII A
 out_1:
 	add r4, r4, r5
-	ld r5, r0, 3 ; BG/FG color
-	or r4, r4, r5 ; Add BG/FG color to character
-	st r2, r4, 0 ; Write character to display
-	and r4, r3, 0xF ; Least significant nibble
-	sub r5, r4, 9
-	brg hex_out2
-	ld r5, r0, 2 ; ASCII 0
+	ld r5, r0, 3 
+	or r4, r4, r5
+	st r2, r4, 0
+	and r4, r3, 15
+	add r5, r4, -9
+	bgt hex_out2
+	ld r5, r0, 2
 	br out_2
 hex_out2:
-	ld r5, r0, 3 ; ASCII A
+	ld r5, r0, 3
 out_2:
 	add r4, r4, r5
-	ld r5, r0, 3 ; BG/FG color
-	or r4, r4, r5 ; Add BG/FG color to character
-	st r2, r4, 0 ; Write character to display
-	br main
-	
-// ===========================================================================
-
-0100 0100 0000 0000
-0100 1000 0000 0001
-0100 1100 1000 0000
+	ld r5, r0, 3
+	or r4, r4, r5
+	st r2, r4, 0
+	br start
