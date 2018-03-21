5d33ff3438df7864e7e9f1bc742be6da17f6708e
Mooneer Salem <mooneer@gmail.com>
2018-03-20 20:01:55 -0700

    Add comments to keyboard test app.

--- example_apps/print_kbd.s
+++ example_apps/print_kbd.s
@@ -1,34 +1,34 @@
 .raw 0xC000				# Keyboard scan code register
 .raw 0xF82F				# Video memory offset
-.raw 0x0700				# BG/FG color
+.raw 0x0730				# ASCII 0
+.raw 0x0741				# ASCII A
 
 start:
-	ld r1, r0, 0
-	ld r2, r0, 1
-	ld r3, r1, 0
-	shr r4, r3, 4
-	and r4, r4, 15
-	add r5, r4, -9
-	bgt hex_out1
-	ld r5, r0, 2 		# ASCII 0
-	br out_1
+	ld r1, r0, 0			# Load scan code address into r1
+	ld r2, r0, 1			# Load video memory address into r2
+loop:
+	ld r3, r1, 0			# Load keyboard scan code into r3
+	and r4, r3, 15			# r4 = r3 && 0xF (isolate last four bits)
+	add r4, r4, -9			# r4 = r4 - 9
+	bgt hex_out1			# if (r4 > 0) jump to hex_out1
+	add r4, r4, 9			# r4 = r4 + 9
+	ld r5, r0, 2			# Load ASCII 0 into r5
+	br out_1			# Jump to out_1
 hex_out1:
-	ld r5, r0, 3		# ASCII A
+	ld r5, r0, 3			# Load ASCII A into r5
 out_1:
-	add r4, r4, r5
-	ld r5, r0, 3 
-	or r4, r4, r5
-	st r2, r4, 0
-	and r4, r3, 15
-	add r5, r4, -9
-	bgt hex_out2
-	ld r5, r0, 2
-	br out_2
+	add r4, r4, r5			# r4 = r4 + r5
+	st r2, r4, 1			# Store r4 at video memory + 1
+	shr r4, r3, 4			# Shift r3 right by 4 bits
+	and r4, r3, 15			# r4 = r3 && 0xF (isolate last four bits)
+	add r4, r4, -9			# r4 = r4 - 9
+	bgt hex_out2			# if (r4 > 0) jump to hex_out2 jump to hex_out2
+	add r4, r4, 9			# r4 = r4 + 9
+	ld r5, r0, 2			# Load ASCII 0 into r5
+	br out_2			# Jump to out_2
 hex_out2:
-	ld r5, r0, 3
+	ld r5, r0, 3			# Load ASCII A into r5
 out_2:
-	add r4, r4, r5
-	ld r5, r0, 3
-	or r4, r4, r5
-	st r2, r4, 0
-	br start
+	add r4, r4, r5			# r4 = r4 + r5
+	st r2, r4, 0			# Store r4 at video memory + 0
+	br loop				# Jump to loop
